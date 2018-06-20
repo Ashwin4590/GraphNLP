4949f3feb2be320612bda9c8c04b7370d692ab26
Ashwin Ramesh <ashwin.pesitcs@gmail.com>
2018-06-20 10:47:41 -0700

    Adding python code, graphs folder, and graphutils java

---
+++ java/src/main/java/graphstore/GraphUtils.java
@@ -0,0 +1,26 @@
+package graphstore;
+
+import java.util.*;
+
+/**
+ * Created by ashwinramesh on 6/19/18.
+ */
+public class GraphUtils {
+    public static <K, V> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new LinkedList<>(map.entrySet());
+        Collections.sort(list, new Comparator<Object>() {
+            @SuppressWarnings("unchecked")
+            public int compare(Object o1, Object o2) {
+                return ((Comparable<V>) ((Map.Entry<K, V>) (o2)).getValue()).compareTo(((Map.Entry<K, V>) (o1)).getValue());
+            }
+        });
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Iterator<Map.Entry<K, V>> it = list.iterator(); it.hasNext();) {
+            Map.Entry<K, V> entry = (Map.Entry<K, V>) it.next();
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+    }
+}
